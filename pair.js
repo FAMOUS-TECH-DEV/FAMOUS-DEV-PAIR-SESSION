897533745216a010f6e8af26670d7cfe3439bcab
FAMØUS DEV 👑 <whitebos2.0@gmail.com>
2025-08-11 05:36:22 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -33,7 +33,7 @@ router.get('/', async (req, res) => {
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: ["Safari (Linux)", "browser", "1.0.0"],
+                browser: ["Chrome (Linux)", "browser", "1.0.0"],
              });
              if(!Pair_Code_By_Maher_Zubair.authState.creds.registered) {
                 await delay(1500);
